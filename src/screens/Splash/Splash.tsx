b008741061cdf6b994add14d4f8226c212ec8898
wmacall15 <wmacall@applaudostudios.com>
2021-08-16 22:28:09 -0600

    Added splash screen initial config and implemented on iOS

--- src/screens/Splash/Splash.tsx
+++ src/screens/Splash/Splash.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import {AuthRoutes, HomeRoutes} from '@routes';
 import {AppState} from 'store';
+import RNBootSplash from 'react-native-bootsplash';
 
 export const Splash = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  });
+
   const {token} = useSelector(({user: {user}}: AppState) => ({
     token: user.token,
   }));
